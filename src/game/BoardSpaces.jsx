79fbc9b6bc1d8d11485fa24c9625a068fac810fc
alex sanchez <alexjsanchez1020@gmail.com>
2024-10-07 21:31:49 -0400

    create board spaces

---
+++ src/game/BoardSpaces.jsx
@@ -0,0 +1,28 @@
+const boardSpaces = {
+    1: {
+        name: "Venice Beach USA",
+        price: 50,
+    },
+    2: {
+        name: "Skate Trivia",
+        price: 0,
+        trivia: true
+    },
+    3: {
+        name: "Burnside",
+        price: 50,
+    },
+    4: {
+        name: "New Setup",
+        price: 100,
+    },
+    5: {
+        name: "Element Skateboards",
+        price: 200,
+    },
+    6: {
+        name: "Shaw Millenium",
+        price: 100,
+    }
+
+}
